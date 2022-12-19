a8a2438f7790711dfb4c9fbe89dd92790ffffe4c
Raivis Petrovskis <rapet80@gmail.com>
2022-12-19 11:06:26 +0000

    add template

--- DJANGO_PROJECT/settings.py
+++ DJANGO_PROJECT/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PROJ_DIR = os.path.join(BASE_DIR, 'DJANGO_PROJECT')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist
@@ -52,7 +53,7 @@ ROOT_URLCONF = 'DJANGO_PROJECT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PROJ_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +120,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- home/views.py
+++ home/views.py
@@ -1,13 +1,6 @@
 
 from django.shortcuts import render
-from django.conf import settings
-from django.core.mail import send_mail
 
-# Create your views here.
-from django.http import HttpResponse
-import datetime
 
 def home(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now 
-    return HttpResponse(html)
+    return render(request, 'home.html')
