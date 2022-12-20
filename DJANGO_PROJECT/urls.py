a68ac40103b780a3173d28bd9de6fc09cc0b78f1
Raivis Petrovskis <rapet80@gmail.com>
2022-12-20 00:45:51 +0000

    add landing page base

--- DJANGO_PROJECT/urls.py
+++ DJANGO_PROJECT/urls.py
@@ -22,4 +22,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
