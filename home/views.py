bbe1371421bfaf088e532e9cef91cf3807dd6461
Raivis Petrovskis <rapet80@gmail.com>
2023-01-19 19:41:08 +0000

    add message view

--- home/views.py
+++ home/views.py
@@ -1,6 +1,30 @@
-
+from django.views.decorators.http import require_POST
+from django.core.mail import send_mail
 from django.shortcuts import render
+from django.conf import settings
 
 
 def home(request):
     return render(request, 'home.html')
+
+@require_POST
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+        
+        message_body = f'Name: {name}\nEmail: {email}\nMessage: {message}'
+
+        # Send email
+        send_mail(
+            f"New contact form submission from {name}", # subject
+            message_body, # message
+            email, # from email
+            [settings.EMAIL_HOST_USER], # to email
+            fail_silently=False,
+        )
+    
+    return render(request, 'home.html')
+            
+             
