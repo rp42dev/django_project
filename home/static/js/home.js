f52583f69b88ff94e19d053728f0a84ab692be14
Raivis Petrovskis <rapet80@gmail.com>
2023-03-06 19:02:57 +0000

    add content

--- home/static/js/home.js
+++ home/static/js/home.js
@@ -4,32 +4,32 @@
     let marquee = document.querySelectorAll('.marquee__text');
 
     // added event listener because it doesn't get the right width
-    addEventListener("load", function () {
-        marquee.forEach(el => {
-            // set a default rate, the higher the value, the faster it is
-            let rate = 100;
-            // get the width of the element
-            let distance = el.offsetWidth;
-            // get the margin-right of the element
-            let style = window.getComputedStyle(el);
-            let marginRight = parseInt(style.marginRight) || 8;
-            // get the total width of the element
-            let totalDistance = distance + marginRight;
-            // get the duration of the animation 
-            // for a better explanation, see the quoted codepen in the first comment
-            let time = totalDistance / rate;
-            // get the parent of the element
-            let container = el.parentElement;
 
-            gsap.to(container, time, {
-                repeat: -1,
-                x: '-' + totalDistance,
-                ease: Linear.easeNone,
-            });
+    marquee.forEach(el => {
+        // set a default rate, the higher the value, the faster it is
+        let rate = 100;
+        // get the width of the element
+        let distance = el.offsetWidth;
+        // get the margin-right of the element
+        let style = window.getComputedStyle(el);
+        let marginRight = parseInt(style.marginRight) || 8;
+        // get the total width of the element
+        let totalDistance = distance + marginRight;
+        // get the duration of the animation 
+        // for a better explanation, see the quoted codepen in the first comment
+        let time = totalDistance / rate;
+        // get the parent of the element
+        let container = el.parentElement;
+
+        gsap.to(container, time, {
+            repeat: -1,
+            x: '-' + totalDistance,
+            ease: Linear.easeNone,
         });
     });
 
-   
+
+
     const zoomElems = document.querySelectorAll('.zoom');
 
     zoomElems.forEach((zoomElem) => {
