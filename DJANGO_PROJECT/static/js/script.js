13d68ae9775a78e4b1c972ce425798bb2e8be08a
Raivis Petrovskis <rapet80@gmail.com>
2023-03-07 15:32:12 +0000

    add content

--- DJANGO_PROJECT/static/js/script.js
+++ DJANGO_PROJECT/static/js/script.js
@@ -18,29 +18,35 @@ $(window).on('load', function () {
 
     let marquee = document.querySelectorAll('.marquee__text');
 
-    // added event listener because it doesn't get the right width
     marquee.forEach(el => {
-        // set a default rate, the higher the value, the faster it is
-        let rate = 200;
-        // get the width of the element
-        let distance = el.clientWidth;
-        // get the margin-right of the element
-        let style = window.getComputedStyle(el);
-        let marginRight = parseInt(style.marginRight) || 0;
-        // get the total width of the element
-        let totalDistance = distance + marginRight;
-        // get the duration of the animation 
-        // for a better explanation, see the quoted codepen in the first comment
-        let time = totalDistance / rate;
-        // get the parent of the element
-        let container = el.parentElement;
 
-        gsap.to(container, time, {
+        let text = el.innerHTML;
+        el.innerHTML = text + text + text;
+        let textWidth = el.scrollWidth;
+        let textSpeed = el.getAttribute('data-speed');
+
+        let textDirection = el.getAttribute('data-direction');
+
+        if (textSpeed == null) {
+            textSpeed = 15;
+        }
+
+
+        const tl = gsap.timeline({
             repeat: -1,
-            x: '-' + totalDistance,
-            ease: Linear.easeNone,
+            ease: 'none',
+            yoyo: true,
         });
+        
+
+        tl.to(el, {
+            x: textDirection == 'right' ? textWidth : -textWidth,
+            duration: textSpeed,
+            ease: 'none',
+        });
+            
     });
+
 
 
 
