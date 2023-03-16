dc9513710c38993f7d69286f8fb870f920371cbd
Raivis Petrovskis <rapet80@gmail.com>
2023-03-16 13:58:31 +0000

    change content

--- home/urls.py
+++ home/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('responsive-design/', views.responsive_deign, name='responsive-design'),
     path('fromladstodads/', views.fromladstodads, name='from-lads-to-dads'),
-    path('web-development/', views.web_development, name='web-development'),
+    path('custom-development/', views.web_development, name='custom-development'),
     path('web-transform/', views.web_transform, name='web-transform')
 ]
